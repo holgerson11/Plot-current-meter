0b7c2a20af1a0c87ff01e760be8dc9c7563f4995
Geo01 <lgeo@gau.fugro.com>
2021-11-20 15:41:52 +0400

    Add cut off value for minimum push time
    This allows for better control of maximum depth delta

--- plotCurrentMeterData.py
+++ plotCurrentMeterData.py
@@ -42,8 +42,10 @@ projectname = '2AFRICA FAW'  # name of your project for output i.e. 2AF East E14
 currentmeter_model = 1          # 0 = Nortek Aquadopp, 1 = Midas ECM
 
 # CUT OFF VALUES (in [m])
-max_depth_delta = 0.3            # Values shallower than maximum depth of file by this value will be used
-max_depth_noise = 0.25           # Cutoff value for noise in depth on seabed
+max_depth_delta = 3             # values shallower than maximum depth of file by this value will be used
+max_depth_noise = 0.05          # cutoff value for noise in depth on seabed
+# CUT OF VALUES (in [s])
+min_push_length = 60            # min time in seconds on seabed to count as individual push
 
 # todo add debug mode
 # debug_file = r'E:\02_Projekte\2Africa East E14\src\plotCurrentMeterData\out\2AF East KWI02_debug table.csv'
@@ -96,14 +98,12 @@ for file in f:
     # todo read debug file here for manual start stop change
 
     # FIND INDIVIDUAL PUSHES
-    df['Diff'] = df['Depth'].diff()     # TODO explain why this works -.-
-    df['OnSeabed'] = df.loc[(df['Diff'] < max_depth_noise) &
-                            (df['Diff'] > -max_depth_noise) &
-                            (df['Depth'] > df['Depth'].max() - max_depth_delta)]['Depth']
-
-    df['ID'] = df['OnSeabed'].apply(lambda x: False if (np.isnan(x)) else True)
-    df['Push'] = df['ID'].ne(df['ID'].shift(1)).cumsum()
-    df['Push'] = np.where(np.isnan(df['OnSeabed']), 0, df['Push'])
+    df['Diff'] = df['Depth'].diff()     # get depth diff between lines
+    df['OnSeabed'] = df.loc[(df['Diff'] < max_depth_noise) & (df['Diff'] > - max_depth_noise)]['Depth']     # depth noise cut off
+    df['OnSeabed'] = np.where((df['Depth'] < df['Depth'].max() - max_depth_delta), np.nan, df['OnSeabed'])  # max depth cut off
+    df['ID'] = df['OnSeabed'].apply(lambda x: False if (np.isnan(x)) else True)     # add bool for CPT on seabed
+    df['Push'] = df['ID'].ne(df['ID'].shift(1)).cumsum()                            # add numbers to events
+    df['Push'] = np.where(np.isnan(df['OnSeabed']), 0, df['Push'])                  # categorize events
 
     df_pushes = df.where(df['Push'] != 0).groupby('Push')
 
@@ -111,78 +111,79 @@ for file in f:
     station_char = ''
 
     for name, group in df_pushes:                           # todo break if more then 27 pushes
-        start_push = group['Date/Time'].iloc[0]
-        stop_push = group['Date/Time'].iloc[-1]
+        if len(group) >= min_push_length:
+            start_push = group['Date/Time'].iloc[0]
+            stop_push = group['Date/Time'].iloc[-1]
 
-        # GET RAW FILE LINE NUMBER
-        start_push_int = df.index.get_loc(start_push) + (line_offset + 1)   # todo test for Nortek
-        stop_push_int = df.index.get_loc(stop_push) + (line_offset + 1)
+            # GET RAW FILE LINE NUMBER
+            start_push_int = df.index.get_loc(start_push) + (line_offset + 1)   # todo test for Nortek
+            stop_push_int = df.index.get_loc(stop_push) + (line_offset + 1)
 
-        # GET STATION NAME
-        if len(df_pushes.groups) > 1 and push_counter > 0:
-            station_char = ascii_uppercase[push_counter - 1]
-        station = os.path.basename(file).split('.')[0] + station_char
+            # GET STATION NAME
+            if len(df_pushes.groups) > 1 and push_counter > 0:
+                station_char = ascii_uppercase[push_counter - 1]
+            station = os.path.basename(file).split('.')[0] + station_char
 
-        # DEBUG FILE LINE
-        debug_table.append([station, start_push_int, stop_push_int, start_push, stop_push])
-        push_counter += 1
+            # DEBUG FILE LINE
+            debug_table.append([station, start_push_int, stop_push_int, start_push, stop_push])
+            push_counter += 1
 
-        file_name = os.path.basename(file)
+            file_name = os.path.basename(file)
 
-        # Calculate velocity vector for MIDAS ECM
-        if currentmeter_model == 1:
-            df['Speed'] = np.hypot(df['Velocity Y'].loc[start_push:stop_push], df['Velocity X'].loc[start_push:stop_push])
+            # Calculate velocity vector for MIDAS ECM
+            if currentmeter_model == 1:
+                df['Speed'] = np.hypot(df['Velocity Y'].loc[start_push:stop_push], df['Velocity X'].loc[start_push:stop_push])
 
-        depth = int(round(np.mean(df['Depth'].loc[start_push:stop_push]), 0))      # Mean depth
-        temp_c = round(np.mean(df['Temperature'].loc[start_push:stop_push]), 1)    # Mean temperature
-        avg_spe = round(df['Speed'].loc[start_push:stop_push].mean(), 2)           # Avg. speed
-        avg_dir = round(np.rad2deg(circmean(np.deg2rad(df['Direction'].loc[start_push:stop_push]))), 1)    # Mean direction
-        sv = round(np.mean(df['Sound Velocity'].loc[start_push:stop_push]), 1)                             # Mean sv
+            depth = int(round(np.mean(df['Depth'].loc[start_push:stop_push]), 0))      # Mean depth
+            temp_c = round(np.mean(df['Temperature'].loc[start_push:stop_push]), 1)    # Mean temperature
+            avg_spe = round(df['Speed'].loc[start_push:stop_push].mean(), 2)           # Avg. speed
+            avg_dir = round(np.rad2deg(circmean(np.deg2rad(df['Direction'].loc[start_push:stop_push]))), 1)    # Mean direction
+            sv = round(np.mean(df['Sound Velocity'].loc[start_push:stop_push]), 1)                             # Mean sv
 
-        # VALUES FOR PLOT
-        avg_dir_plot = circmean(np.deg2rad(df['Direction'].loc[start_push:stop_push]))
-        avg_spe_plot = df['Speed'].loc[start_push:stop_push].mean()
-        std_dir = circstd(np.deg2rad(df['Direction'].loc[start_push:stop_push]))
+            # VALUES FOR PLOT
+            avg_dir_plot = circmean(np.deg2rad(df['Direction'].loc[start_push:stop_push]))
+            avg_spe_plot = df['Speed'].loc[start_push:stop_push].mean()
+            std_dir = circstd(np.deg2rad(df['Direction'].loc[start_push:stop_push]))
 
-        # DATE/TIME for .csv
-        date = df['Date/Time'].dt.date.loc[start_push]
-        time = df['Date/Time'].dt.time.loc[start_push]
+            # DATE/TIME for .csv
+            date = df['Date/Time'].dt.date.loc[start_push]
+            time = df['Date/Time'].dt.time.loc[start_push]
 
-        # VALUES FOR .csv
-        summary_table.append([station, file_name, date, time, depth, temp_c, avg_spe, avg_dir, sv])
+            # VALUES FOR .csv
+            summary_table.append([station, file_name, date, time, depth, temp_c, avg_spe, avg_dir, sv])
 
-        # SET POLAR PLOT
-        fig = plt.figure(figsize=(8, 8))
-        ax = fig.add_subplot(111, projection='polar')
-        ax.set_theta_direction('clockwise')
-        ax.set_theta_offset(0.5 * np.pi)
+            # SET POLAR PLOT
+            fig = plt.figure(figsize=(8, 8))
+            ax = fig.add_subplot(111, projection='polar')
+            ax.set_theta_direction('clockwise')
+            ax.set_theta_offset(0.5 * np.pi)
 
-        x = np.deg2rad(df['Direction'].loc[start_push:stop_push])
-        y = df['Speed'].loc[start_push:stop_push]
+            x = np.deg2rad(df['Direction'].loc[start_push:stop_push])
+            y = df['Speed'].loc[start_push:stop_push]
 
-        ax.scatter(x, y, c='red')       # todo add color scale for points for comparability
-        ax.bar(avg_dir_plot, avg_spe_plot, width=std_dir, bottom=0.0, alpha=0.5, color='red')
+            ax.scatter(x, y, c='red')       # todo add color scale for points for comparability
+            ax.bar(avg_dir_plot, avg_spe_plot, width=std_dir, bottom=0.0, alpha=0.5, color='red')
 
-        degreechar = u'\N{DEGREE SIGN}'
-        ax.set_xlabel('Direction [%s]' % degreechar)
-        ax.set_ylabel('Current speed [m/s]')
-        ax.yaxis.labelpad = 35
+            degreechar = u'\N{DEGREE SIGN}'
+            ax.set_xlabel('Direction [%s]' % degreechar)
+            ax.set_ylabel('Current speed [m/s]')
+            ax.yaxis.labelpad = 35
 
-        plt.suptitle(station)
-        ax.set_title('Mean direction: %.0f%s Mean velocity: %.2f m/s Std. dev: %.2f%s' % (
-            avg_dir, degreechar, avg_spe, np.rad2deg(std_dir), degreechar))
-        plt.tight_layout()
-        plot_save = os.path.join(out_dir, station + '.png')
-        plt.savefig(plot_save)
-        # plt.show()
+            plt.suptitle(station)
+            ax.set_title('Mean direction: %.0f%s Mean velocity: %.2f m/s Std. dev: %.2f%s' % (
+                avg_dir, degreechar, avg_spe, np.rad2deg(std_dir), degreechar))
+            plt.tight_layout()
+            plot_save = os.path.join(out_dir, station + '.png')
+            plt.savefig(plot_save)
+            # plt.show()
 
-        # OUTPUT
-        print('-' * 69)
-        print('Station:\t\t%s' % station)
-        print('Date/Time:\t\t%s %s' % (date, time))
-        print('Mean direction:\t%.0f%s' % (avg_dir, degreechar))
-        print('Mean velocity:\t%.2f m/s' % avg_spe)
-        print('Std. dev:\t\t%.2f%s' % (np.rad2deg(std_dir), degreechar))
+            # OUTPUT
+            print('-' * 69)
+            print('Station:\t\t%s' % station)
+            print('Date/Time:\t\t%s %s' % (date, time))
+            print('Mean direction:\t%.0f%s' % (avg_dir, degreechar))
+            print('Mean velocity:\t%.2f m/s' % avg_spe)
+            print('Std. dev:\t\t%.2f%s' % (np.rad2deg(std_dir), degreechar))
 
     # DEBUG PLOT
     # todo fix plot and export
@@ -192,26 +193,38 @@ for file in f:
     ax.plot(df['Date/Time'], df['Depth'])
     # ax2 = ax.twinx()
 
-    ax2.plot(df['Date/Time'], df['Diff'], color='green')  # todo subplot for diff plot
+    ax2.plot(df['Date/Time'], df['Diff'], color='green')
+    # todo clean rect plot code
+    # ADD CUT OFF RECTANGLES
+    start = mdates.date2num(df['Date/Time'].index.max())
+    end = mdates.date2num(df['Date/Time'].index.min())
+    y = max_depth_noise
+    width = end - start
+    # x, y, width, height
+    rect = patches.Rectangle((start, df['Depth'].max()), width, max_depth_delta * -1, linewidth=1, edgecolor='r', fill=False)
+    rect2 = patches.Rectangle((start, y), width, max_depth_noise * -2, linewidth=1, edgecolor='r', fill=False)
+    ax.add_patch(rect)
+    ax2.add_patch(rect2)
 
     push_counter = 0
     station_char = ''
     colors = plt.cm.tab10(np.linspace(0, 1, len(df_pushes)))
 
     for name, group in df_pushes:
-        if len(df_pushes.groups) > 1 and push_counter > 0:      # todo station name generation as function
-            station_char = ascii_uppercase[push_counter - 1]
-        station = os.path.basename(file).split('.')[0] + station_char
+        if len(group) >= min_push_length:
+            if len(df_pushes.groups) > 1 and push_counter > 0:      # todo station name generation as function
+                station_char = ascii_uppercase[push_counter - 1]
+            station = os.path.basename(file).split('.')[0] + station_char
 
-        ax.fill_between(group['Date/Time'], group['Depth'], 0, color=colors[push_counter])
-        annotation_x = group['Date/Time'].index.max() - \
-                       (pd.Timedelta(group['Date/Time'].index.max() - group['Date/Time'].index.min()) / 2)
-        annotation_y = (group['Depth'].max() / 2)
+            ax.fill_between(group['Date/Time'], group['Depth'], 0, color=colors[push_counter])
+            annotation_x = group['Date/Time'].index.max() - \
+                           (pd.Timedelta(group['Date/Time'].index.max() - group['Date/Time'].index.min()) / 2)
+            annotation_y = (group['Depth'].max() / 2)
 
-        ax.annotate(station, xy=(annotation_x, annotation_y), ha='center', color='white')
-        push_counter += 1
-        # todo add lines and squares for cut off
-        # todo add grid lines
+            ax.annotate(station, xy=(annotation_x, annotation_y), ha='center', color='white')
+            push_counter += 1
+            # todo add lines and squares for cut off
+            # todo add grid lines
 
     # Y AXIS
     ax.set_ylabel('Depth [m]')
